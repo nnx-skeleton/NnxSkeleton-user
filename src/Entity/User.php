64443552cf7732642f2d7e8fceae10b3d045bf6b
dmitry <plitakov@platformasoft.ru>
2016-04-27 15:15:54 +0300

    add files

---
+++ src/Entity/User.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dmitry
+ * Date: 27.04.16
+ * Time: 14:09
+ */
+namespace NnxMember\User\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Class User
+ * @package NnxMember\User\Entity
+ *
+ * @ORM\Entity()
+ * @ORM\Table(name="users")
+ * @ORM\InheritanceType("SINGLE_TABLE")
+ * @ORM\DiscriminatorColumn(name="type", type="string")
+ * @ORM\DiscriminatorMap(value={ "user" = "User" })
+ */
+class User implements UserInterface
+{
+
+    /**
+     * @var int
+     *
+     * @ORM\Id()
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    protected $id;
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param $id
+     * @return $this
+     */
+    public function setId($id)
+    {
+        $this->id = (int)$id;
+        return $this;
+    }
+}
---
+++ src/Entity/UserInterface.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dmitry
+ * Date: 27.04.16
+ * Time: 14:13
+ */
+namespace NnxMember\User\Entity;
+
+/**
+ * Interface UserInterface
+ * @package NnxMember\User\Entity
+ */
+interface UserInterface
+{
+    public function getId();
+}
