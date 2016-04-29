7befca05c396203a300c26d526efb0564dcf4407
dmitry <plitakov@platformasoft.ru>
2016-04-29 13:44:04 +0300

    add config files for doctrine

--- config/doctrine.config.php
+++ config/doctrine.config.php
@@ -16,6 +16,7 @@ return [
                 'paths' => [__DIR__ . '/../src/Entity'],
                 'class' => \Doctrine\ORM\Mapping\Driver\AnnotationDriver::class,
             ],
+
         ],
     ],
 ];
