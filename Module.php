abb8707ae7d3f062515ae949ae3448c9bf04a809
dmitry <plitakov@platformasoft.ru>
2016-05-04 15:40:27 +0300

    add empty module.php files

---
+++ Module.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dmitry
+ * Date: 04.05.16
+ * Time: 15:37
+ */
+
+class Module {
+
+} 
--- config/doctrine.config.php
+++ config/doctrine.config.php
@@ -6,12 +6,12 @@
  * Time: 12:32
  */
 
-namespace NnxMember\User;
+namespace NnxSkeleton\User;
 
 return [
     'doctrine' => [
         'driver' => [
-            'NnxSkeletonUserEntityUser' => [
+            'NnxSkeletonUserEntity' => [
                 'cache' => 'array',
                 'paths' => [__DIR__ . '/../src/Entity'],
                 'class' => \Doctrine\ORM\Mapping\Driver\AnnotationDriver::class,
